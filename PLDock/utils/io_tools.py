cd1fdb5b9f9396d542b770cf48ec64225875404c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 15:13:11 +0000

    Adding tarfile member sanitization to extractall()

--- PLDock/utils/io_tools.py
+++ PLDock/utils/io_tools.py
@@ -129,7 +129,26 @@ def untar(in_path: str, out_dir: str = None):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     with tarfile.open(in_path) as tar:
-        tar.extractall(path=out_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=out_dir)
         return 1
 
 
